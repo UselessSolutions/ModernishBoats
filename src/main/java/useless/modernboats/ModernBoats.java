fafab510145c319add259a4e4510b3beacc4d451
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-10-28 22:13:21 -0500

    New Pack reg system

--- src/main/java/useless/modernboats/ModernBoats.java
+++ src/main/java/useless/modernboats/ModernBoats.java
@@ -3,24 +3,15 @@ package useless.modernboats;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import turniplabs.halplibe.util.ConfigHandler;
-import useless.modernboats.mixin.PacketMixin;
-
-import java.util.Properties;
+import turniplabs.halplibe.helper.NetworkHelper;
 
 
 public class ModernBoats implements ModInitializer {
     public static final String MOD_ID = "modernboats";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final ConfigHandler config;
-	static {
-		Properties prop = new Properties();
-		prop.setProperty("boat_packet_id", "210");
-		config = new ConfigHandler(MOD_ID, prop);
-	}
     @Override
     public void onInitialize() {
-		PacketMixin.callAddIdClassMapping(config.getInt("boat_packet_id"), false, true, PacketBoatMovement.class);
+		NetworkHelper.register(PacketBoatMovement.class, true, false);
         LOGGER.info("ModernBoats initialized.");
     }
 }
